94bd01264e528290b8b010039f759cd65dfc59fc
Mayank Kumar Sohanda <ms4959@srmist.edu.in>
2024-01-21 02:23:58 +0530

    complete: Interaction for Create an Art Space app

--- Unit-2-Building-App-UI/Interacting-with-UI-and-State/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ Unit-2-Building-App-UI/Interacting-with-UI-and-State/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -18,6 +18,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,6 +31,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,7 +43,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArtSpaceTheme {
-                ArtworkWallPreview()
+                ArtSpaceLayout()
             }
         }
     }
@@ -48,26 +53,62 @@ class MainActivity : ComponentActivity() {
 fun ArtSpaceLayout(
     modifier: Modifier = Modifier
 ) {
+    var currentArtwork by remember {
+        mutableIntStateOf(1)
+    }
+
+    val imageDrawable = when (currentArtwork) {
+        1 -> R.drawable.gradient_smoky_wallpaper
+        2 -> R.drawable.nature_landscape
+        else -> R.drawable.portrait_sketch
+    }
+
+    val imageTitle = when (currentArtwork) {
+        1 -> R.string.gradient_smoky_title
+        2 -> R.string.nature_landscape_title
+        else -> R.string.portrait_sketch_title
+    }
+
+    val imageArtist = when (currentArtwork) {
+        1 -> R.string.gradient_smoky_artist
+        2 -> R.string.nature_landscape_artist
+        else -> R.string.portrait_sketch_artist
+    }
+
     Column(
         modifier = modifier.padding(horizontal = 20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         ArtworkWall(
-            painter = R.drawable.gradient_smoky_wallpaper,
+            painter = imageDrawable,
             modifier = Modifier
                 .padding(bottom = 16.dp, top = 40.dp)
         )
 
         ArtworkDescriptor(
-            title = R.string.gradient_smoky_title,
-            artist = R.string.artist,
+            title = imageTitle,
+            artist = imageArtist,
             modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
         )
 
         DisplayController(
+            onClickNext = {
+                currentArtwork = when (currentArtwork) {
+                    1 -> 2
+                    2 -> 3
+                    else -> 1
+                }
+            },
+            onClickPrevious = {
+                currentArtwork = when (currentArtwork) {
+                    1 -> 3
+                    2 -> 1
+                    else -> 2
+                }
+            },
             modifier = Modifier.fillMaxWidth()
         )
     }
@@ -119,16 +160,19 @@ fun ArtworkDescriptor(
             }
         },
         modifier = modifier
+            .fillMaxWidth(),
+        textAlign = TextAlign.Center
     )
 }
 
 @Composable
 fun ControllerButton(
     @StringRes label: Int,
+    onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Button(
-        onClick = { },
+        onClick = onClick,
         modifier = modifier
             .size(width = 136.dp, height = 36.dp)
     ) {
@@ -140,6 +184,8 @@ fun ControllerButton(
 
 @Composable
 fun DisplayController(
+    onClickNext: () -> Unit,
+    onClickPrevious: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Row(
@@ -149,11 +195,13 @@ fun DisplayController(
         verticalAlignment = Alignment.CenterVertically
     ) {
         ControllerButton(
-            label = R.string.previous_image_button
+            label = R.string.previous_image_button,
+            onClick = onClickPrevious
         )
 
         ControllerButton(
             label = R.string.next_image_button,
+            onClick = onClickNext,
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentWidth(Alignment.End)
